964172f7f5e34887c979a0e8e37267dd788282d2
Vinny Smith <89861748+mistervinster@users.noreply.github.com>
2021-10-24 23:27:24 -0600

    Learning Vue Again, trying to get things to work

--- frontend/pages/drinks/index.vue
+++ frontend/pages/drinks/index.vue
@@ -1,10 +1,31 @@
 <template>
-  <Nav/>
+  <div>
+    <Nav/>
+    <div v-for="drink in drinks" :key="drink.name">
+    <b-card
+        :title="drinks.Name"
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+    >
+            <b-card-text>
+            ${{drink.price}}
+            {{drink.description}}
+            {{drink.type}}
+            </b-card-text>
+
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+    </b-card>
+        </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+    async asyncData({ $axios, $config }) {
+            const drinks = await $axios.$get('/api/drinks/')
+            return { drinks }
+        }
 }
 </script>
 
