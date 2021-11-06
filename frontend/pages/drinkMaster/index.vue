5d114608273fe7f55d1ed3051cea971e8bd1199c
Vinny Smith <89861748+mistervinster@users.noreply.github.com>
2021-11-06 11:56:59 -0600

    did it again

---
+++ frontend/pages/drinkMaster/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Nav />
+        <div>
+            <b-button href="/drinkMaster/create" class="mt-3">
+                Add drink
+            </b-button>
+        </div>
+        <div v-for="drink in drinks" :key="drink.id">
+            {{ drink }} 
+            <b-button class="mt-3"
+                variant="danger"
+                @click="delTournament(tournament)">
+                Delete
+            </b-button>
+            <b-button class="mt-3"
+                variant="secondary"
+                :href="'/tournaments/edit/' + tournament.id">
+                
+                Edit
+            </b-button>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    async asyncData({ $axios, $config }) {
+            const drinks = await $axios.$get('/api/drinks/')
+            return { drinks }
+        }
+}
+</script>
+
+<style>
+
+</style>
